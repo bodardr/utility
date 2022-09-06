df1b00a2d84f82f9f04f2dfe545e9c66833c6a52
Romain Bodard <romain.bodard@videotron.ca>
2022-09-06 10:07:08 -0400

    Introduced more Component Extensions, and now includes inactive components by default.

--- Runtime/Scripts/ComponentExtensions.cs
+++ Runtime/Scripts/ComponentExtensions.cs
@@ -8,19 +8,40 @@ namespace Bodardr.Utility.Runtime
         /// <summary>
         /// Taken inspiration from : https://answers.unity.com/questions/863509/how-can-i-find-all-objects-that-have-a-script-that.html
         /// </summary>
+        /// <param name="includeInactive"></param>
         /// <typeparam name="T">The type of component to fetch</typeparam>
         /// <returns>All components across all scenes, containing this class or interface type.</returns>
-        public static T[] FindComponentsOfTypeInAllScenes<T>()
+        public static List<T> FindComponentsInAllActiveScenes<T>(bool includeInactive = true)
         {
             List<T> components = new List<T>();
 
             for (int i = 0; i < SceneManager.sceneCount; i++)
             {
                 foreach (var rootGO in SceneManager.GetSceneAt(i).GetRootGameObjects())
-                    components.AddRange(rootGO.GetComponentsInChildren<T>());
+                    components.AddRange(rootGO.GetComponentsInChildren<T>(includeInactive));
             }
 
-            return components.ToArray();
+            return components;
+        }
+
+        public static List<T> FindComponentsInActiveScene<T>(bool includeInactive = true)
+        {
+            List<T> components = new List<T>();
+
+            foreach (var rootGO in SceneManager.GetActiveScene().GetRootGameObjects())
+                components.AddRange(rootGO.GetComponentsInChildren<T>(includeInactive));
+
+            return components;
+        }
+
+        public static List<T> FindComponentsInScene<T>(Scene scene, bool includeInactive = true)
+        {
+            List<T> components = new List<T>();
+
+            foreach (var rootGO in scene.GetRootGameObjects())
+                components.AddRange(rootGO.GetComponentsInChildren<T>(includeInactive));
+
+            return components;
         }
     }
 }
