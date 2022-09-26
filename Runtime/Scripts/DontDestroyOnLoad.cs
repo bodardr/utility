e9ee01ab17459891252eb47ed525fd0af1a2db66
Romain Bodard <romain.bodard@videotron.ca>
2022-09-26 10:58:41 -0400

    Commented the DontDestroyOnLoad.cs class

--- Runtime/Scripts/DontDestroyOnLoad.cs
+++ Runtime/Scripts/DontDestroyOnLoad.cs
@@ -2,6 +2,12 @@
 
 namespace Bodardr.UI.Runtime
 {
+    /// <summary>
+    /// Inherit this class for use in class singletons that are self-initialized
+    /// and where its lifecycle consists of the whole session.
+    /// This component must be in a scene.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class DontDestroyOnLoad<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T instance;
@@ -17,17 +23,17 @@ namespace Bodardr.UI.Runtime
             }
         }
 
+        private void OnDestroy()
+        {
+            if (instance == this)
+                instance = null;
+        }
+
         private static void CreateInstance()
         {
             GameObject go = new GameObject(typeof(T).Name, typeof(T));
             instance = go.GetComponent<T>();
             DontDestroyOnLoad(go);
-        }
-
-        private void OnDestroy()
-        {
-            if (instance == this)
-                instance = null;
         }
     }
 }
