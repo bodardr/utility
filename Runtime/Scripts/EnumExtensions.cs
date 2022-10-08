2179636fe49a6c2fcd1ff86436a8ba9e64a8d45f
Romain Bodard <romain.bodard@videotron.ca>
2022-10-07 21:13:21 -0400

    Added a Flags to Array for enums.

--- Runtime/Scripts/EnumExtensions.cs
+++ Runtime/Scripts/EnumExtensions.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Linq;
 using Random = UnityEngine.Random;
 
 namespace Bodardr.Utility.Runtime
 {
-    public class EnumExtensions
+    public static class EnumExtensions
     {
         public static TEnum RandomEnumValue<TEnum>() where TEnum : Enum
         {
             var values = Enum.GetValues(typeof(TEnum));
             var index = Random.Range(0, values.Length);
             return (TEnum)values.GetValue(index);
+        }
+
+        public static TEnum[] FlagsToArray<TEnum>(this TEnum val) where TEnum : Enum
+        {
+            if (!val.HasFlag(val))
+                return Array.Empty<TEnum>();
+
+            var intValue = (int)(object)val;
+            var values = Enum.GetValues(typeof(TEnum)).Cast<int>();
+            return values.Where(x => (x & intValue) != 0).Cast<TEnum>().ToArray();
         }
     }
 }
