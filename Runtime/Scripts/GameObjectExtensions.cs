43f5ed8eb9a045a9e03924d25e498ac39300b2e2
Romain Bodard <romain.bodard@videotron.ca>
2022-11-06 00:58:01 -0400

    GetFullPath for scene path and GetChildren for child transforms.

--- Runtime/Scripts/GameObjectExtensions.cs
+++ Runtime/Scripts/GameObjectExtensions.cs
@@ -5,9 +5,9 @@ namespace Bodardr.Utility.Runtime
     public static class GameObjectExtensions
     {
         /// <returns>True if this GameObject is in the <b>DontDestroyOnLoad</b> Scene</returns>
-        public static bool IsDontDestroyOnLoad(this GameObject gameObject)
-        {
-            return gameObject.scene.name == "DontDestroyOnLoad";
-        }
+        public static bool IsDontDestroyOnLoad(this GameObject gameObject) => gameObject.scene.name == "DontDestroyOnLoad";
+        
+        /// <returns>The full scene path of this GameObject</returns>
+        public static string GetFullPath(this GameObject gameObject) => gameObject.transform.GetFullPath();
     }
 }
--- Runtime/Scripts/TransformExtensions.cs
+++ Runtime/Scripts/TransformExtensions.cs
@@ -1,23 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Text;
+using UnityEngine;
 
-    public static class TransformExtensions
+public static class TransformExtensions
+{
+    public static Transform FindWithTag(this Transform tr, string tag)
     {
-        public static Transform FindWithTag(this Transform tr, string tag)
+        foreach (Transform transform in tr)
         {
-            foreach (Transform transform in tr)
-            {
-                if (transform.CompareTag(tag))
-                    return transform;
+            if (transform.CompareTag(tag))
+                return transform;
 
-                if (transform.childCount <= 0)
-                    continue;
-                
-                var val = transform.FindWithTag(tag);
-                    
-                if (val)
-                    return val;
-            }
+            if (transform.childCount <= 0)
+                continue;
 
-            return null;
+            var val = transform.FindWithTag(tag);
+
+            if (val)
+                return val;
         }
+
+        return null;
     }
+
+    public static Transform[] GetChildren(this Transform tr)
+    {
+        var children = new Transform[tr.childCount];
+
+        for (int i = 0; i < children.Length; i++)
+            children[i] = tr.GetChild(i);
+
+        return children;
+    }
+
+    /// <returns>The full scene path of this GameObject</returns>
+    public static string GetFullPath(this Transform tr)
+    {
+        var parents = tr.GetComponentsInParent<Transform>();
+
+        var str = new StringBuilder(parents[^1].name);
+        for (var i = parents.Length - 2; i >= 0; i--)
+            str.Append($"/{parents[i].name}");
+
+        return str.ToString();
+    }
+}
